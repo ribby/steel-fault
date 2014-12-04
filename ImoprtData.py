fbeda9c24326d59cf631a238a01bee5314ed0b6e
ribby <alex.rybchuk@gmail.com>
2014-12-03 20:54:22 -0500

    Further cleaned up data. Tried PCA but will save for later date. Set up DT -> acc = 0.7

--- ImoprtData.py
+++ ImoprtData.py
@@ -9,6 +9,10 @@ import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import svm
 
 index = range(0,1940)
 col = ['X_Minimum', 'X_Maximum', 'Y_Minimum', 'Y_Maximum', 'Pixels_Areas', 
@@ -25,10 +29,32 @@ df = pd.DataFrame.from_csv('steel.txt', sep='\t')
 # Replaces X_Min as index with 0...n
 df = df.reset_index()
 
-# Probably need to do feature scaling
+# Break up and convert data
+X = df[col[0:27]]
+X = X.as_matrix()
+
+y = df[col[27:]]
+y = y.as_matrix()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, 
+                                                    random_state=123)
+                                                    
+# Feature scaling
+std_scale = preprocessing.StandardScaler().fit(X_train)
+X_train = std_scale.transform(X_train)
+X_test = std_scale.transform(X_test)
 
 #PCA
+#sklearn_pca = PCA(n_components=2)
+#transf_pca = sklearn_pca.fit_transform(X_train)
+#print transf_pca
 
-#X_train, X_test, y_train, y_test = train_test_split(X_wine, y_wine,
-#    test_size=0.30, random_state=123)
+# DT
+clf = DecisionTreeClassifier()
+clf.fit(X_train,y_train)
+pred = clf.predict(X_test)
+
+acc = accuracy_score(y_test, pred)
+print "The DT accuracy is",acc
+
 
